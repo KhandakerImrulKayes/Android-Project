8902ad3adffec17cefcd9cb172f0f967a0132246
Khandaker Imrul Kayes <145383284+KhandakerImrulKayes@users.noreply.github.com>
2025-01-01 22:39:28 +0600

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,84 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+//    id 'com.android.application'
+//    id 'org.jetbrains.kotlin.android'
+//    id 'kotlin-kapt'
 }
+
+android {
+    namespace = "com.example.mplcricket"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.mplcricket"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // Firebase Libraries
+    implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-messaging")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-ads")
+    implementation("com.google.android.gms:play-services-auth:20.0.1")  // Google Sign-In SDK
+    implementation("com.google.firebase:firebase-auth:21.1.0")  // Firebase Auth SDK
+
+    // Additional libraries
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation("com.squareup.picasso:picasso:2.8")
+    implementation("com.google.code.gson:gson:2.10")
+    implementation("org.jsoup:jsoup:1.16.1")
+
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.5")
+    implementation("androidx.viewpager2:viewpager2:1.1.0")
+
+    // Room dependencies
+//    implementation("androidx.room:room-runtime:2.6.1")
+//    kapt("androidx.room:room-compiler:2.6.1")
+//    implementation("androidx.room:room-ktx:2.6.1")
+//    testImplementation("androidx.room:room-testing:2.5.2")
+}
+
